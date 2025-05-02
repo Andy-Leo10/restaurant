2ffa154855e3e4873df3cfeafff200ed74b4429c
AndresAlamo10 <andres@asimovo.com>
2025-05-02 17:20:53 -0500

    +

--- app/src/main/java/com/example/restaurant/MainActivity.kt
+++ app/src/main/java/com/example/restaurant/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             RestaurantTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "modified",
+                        name = "Andrés",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
